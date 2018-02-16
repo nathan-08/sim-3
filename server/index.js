01ce59b325069f2f46fdcbec844f516bee308cba
Brian Larson <manateeman24@hotmail.com>
2018-02-16 16:15:50 -0700

    basic server establised

--- server/index.js
+++ server/index.js
@@ -2,24 +2,35 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
       session = require('express-session'),
-      config = require('./config'),
+      massive = require('massive'),
+      passport = require ('passport'),
+      Auth0Stratgey = require('passport-auth0'),
       app = express(),
       port = 3018;
 
+      require('dotenv').config()
 
-// ========== MIDDLEWARE ========== //
+
 
 // ===== TOP LEVEL MIDDLEWARE ===== //
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(session({
-     secret: config.secret,
+     secret: process.env.SESSION_SECRET,
      saveUnitialized: false,
      resave: false
 }));
 
 // ===== CUSTOM MIDDLEWARE ===== //
+
+massive(process.env.DB_CONNECTION).then(db => {
+    app.set('db', db)
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 
 
@@ -27,27 +38,22 @@ app.use(session({
 
 // === GET REQUESTS === //
 
-app.get('/api/friends/list', (req, res) => {
-    
-})
-app.get('/api/user/list', (req, res) => {
-    
-})  
-app.get('/api/user/search', (req, res) => {
-    
-})
+app.get('/api/friend/list', )
+app.get('/api/user/list', )  
+
+app.get('/api/user/search', )
 
 
 // === PUT REQUESTS === //
-
+app.put('/api/user/update/:id', )
 
 
 // === POST REQUESTS === //
 
-
-
-// === DELETE REQUESTS === //
-
+app.post('/api/friend/add', )
+app.post('/api/friend/remove', )
+app.post('/api/recommended', )
+app.post('/api/recommdended/add', )
 
 
 app.listen(port, () => {
